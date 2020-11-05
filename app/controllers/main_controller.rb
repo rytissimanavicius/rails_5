0159c4ca31797e7a2826e986e71da8aff3ff6379
Rytis Simanavičius <rytucis.qua@gmail.com>
2020-11-05 23:24:21 +0200

    comments

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,12 +1,15 @@
 class MainController < ApplicationController
+    # Set title in views/main/finder.html.erb template as
     def finder
         @title = "Kelionių paieška"
     end
 
+    # Set title in views/main/contacts.html.erb template as
     def contacts
         @title = "Kontaktai"
     end
 
+    # Set title in views/main/about.html.erb template as
     def about
         @title = "DUK"
     end
--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -1,13 +1,18 @@
 class SuggestionsController < ApplicationController
+    # Before setting the prices set pvm
     before_action :get_pvm
 
     $pvm = 0.00
 
+
+    # Calculate and set the prices for
+    # views/suggestions/suggestions.html.erb template
     def suggestions
         @popular_left_price = '%.2f' % (1200.00 - (1200.00 * $pvm))
         @popular_right_price = '%.2f' % (950.00 - (950.00 * $pvm))
     end
 
+    # Return pvm
     def get_pvm
         $pvm = 0.27
     end
